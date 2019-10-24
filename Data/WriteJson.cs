b9a3c5a5ae77c231aebfa6c5a24e6fb8b4f10c99
Christoffer Lundqvist <39331850+chribbelund@users.noreply.github.com>
2019-10-24 13:22:19 +0200

    massa json skit

---
+++ Data/WriteJson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace Data {
+    public class WriteJson {
+        public void GetRss() {
+
+            var feedUrl = ("https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/10/explicit.rss");
+            XmlDocument rss = new XmlDocument();
+            rss.Load(feedUrl);
+            string json = JsonConvert.SerializeXmlNode(rss);
+            Console.WriteLine(json);
+            File.WriteAllText(feedUrl +".json" , json);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/*
+XmlReader xr = XmlReader.Create("http://www.hotukdeals.com/rss/hot");
+SyndicationFeed feed = SyndicationFeed.Load(xr);
+string json;
+foreach (var item in feed.Items) {
+
+}
+
+return deals;
+*/
