7676b1bbf4c4a3ceee139c0f64356b7be4b245e6
Toastah <39379537+Toastah@users.noreply.github.com>
2019-10-24 15:39:49 +0200

    BLL
    Försökt göra en grund till Kategorier

--- BLL/Class1.cs
+++ BLL/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,43 @@ using Data;
 
 namespace BLL
 {
-    public class Class1
+    public class Class1 {
+        static string dbFile = "data.txt";
+
+    public static void Main(string[] args)
     {
+        CategoryList categorys;
+        if (File.Exists(dbFile))
+        {
+            try
+            {
+                categorys = SerializedReaderWriter.Read<CategoryList>(dbFile);
+
+                    foreach (var category in categorys)
+                {
+                    Console.WriteLine(category.Categorys);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        else
+        {
+            categorys = new CategoryList {
+                    ///Här ska vi mata in strings från GUI
+                };
+            try
+            {
+                SerializedReaderWriter.Write<CategoryList>(dbFile, categorys);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Serialization failed!");
+                Console.WriteLine(e.Message);
+            }
+        }
     }
+}
 }
