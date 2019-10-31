b64a9f20f00c72da3ac2ab56d2b0fc89c88bf6fc
Christoffer Lundqvist <39331850+chribbelund@users.noreply.github.com>
2019-10-31 15:43:05 +0100

    datalagerskit

---
+++ Data/loadPodcasts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace testing {
+    class loadPodcasts {
+
+
+        public string[] getPodcasts() {
+            string[] fileArray = Directory.GetFiles("Podcasts/");
+            foreach (var item in fileArray) {
+                Console.WriteLine(item);
+            }
+            return fileArray;
+        }
+        public void deletePodcast(string filename) {
+            File.Delete(filename);
+        }
+
+    }
+}
