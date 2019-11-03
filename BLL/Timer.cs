863c9a6c01b4f0b4dc7f9459d1b81d992199497a
Toastah <39379537+Toastah@users.noreply.github.com>
2019-11-03 01:17:52 +0100

    Testklass Timer

---
+++ BLL/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace BLL
+{
+    class Timer
+    {
+        private static System.Timers.Timer aTimer;
+
+        public static void Main()
+        {
+            SetTimer();
+
+            Console.ReadLine();
+            aTimer.Stop();
+            aTimer.Dispose();
+
+        }
+
+        private static void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+    }
+}
