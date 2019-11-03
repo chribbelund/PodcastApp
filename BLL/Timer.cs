d6f7aff5613589b8f3b1271336f93320bb46f119
Toastah <39379537+Toastah@users.noreply.github.com>
2019-11-03 18:50:14 +0100

    Update Timer.cs

--- BLL/Timer.cs
+++ BLL/Timer.cs
@@ -11,6 +11,8 @@ namespace BLL
     {
         private static System.Timers.Timer aTimer;
 
+        public static ElapsedEventHandler OnTimedEvent { get; private set; }
+
         public static void Main()
         {
             SetTimer();
@@ -23,12 +25,38 @@ namespace BLL
 
         private static void SetTimer()
         {
-            // Create a timer with a two second interval.
-            aTimer = new System.Timers.Timer(2000);
+            // Create a timer with a twenty second interval.
+            aTimer = new System.Timers.Timer(20000);
             // Hook up the Elapsed event for the timer. 
             aTimer.Elapsed += OnTimedEvent;
             aTimer.AutoReset = true;
             aTimer.Enabled = true;
+        }
+
+        private async void InitializeTimer()
+        {
+            await WaitAndExecute(2000, () =>
+            {
+                var viewmodel = new BandInfoRepository();
+                listView.ItemsSource = viewmodel.BandInfo;
+            });
+        }
+        private async Task WaitAndExecute(int milisec, Action actionToExecute)
+        {
+            await Task.Delay(milisec);
+            actionToExecute();
+        }
+
+        static async Task DoStuffOnTimer()
+        {
+            using (var timer = new TaskTimer(1000).Start())
+            {
+                foreach (var task in timer)
+                {
+                    await task;
+                    DoStuff();
+                }
+            }
         }
     }
 }
